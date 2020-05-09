1c0041b7f103082cc410b9e27a84e4c2e6780b71
Eldar Amantay <eldar.amantay@klarna.com>
2020-05-10 00:00:32 +0200

    Init v0 Pomodoro

---
+++ pomodoro/precache-manifest.771bea387b928554ee26e22f20de1bcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ab54246327a361a33e27d3352e6c86",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "f9a105afce8981ea3641",
+    "url": "/pomodoro/static/css/main.142c0b23.chunk.css"
+  },
+  {
+    "revision": "8ae9d36d8879c537aba0",
+    "url": "/pomodoro/static/js/2.4e8f576d.chunk.js"
+  },
+  {
+    "revision": "f9a105afce8981ea3641",
+    "url": "/pomodoro/static/js/main.82f83334.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  }
+]);
